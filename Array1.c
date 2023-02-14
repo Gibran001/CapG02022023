c5aa8ce4d61a59d33441e40c480bc55251d65136
Gibran001 <bikergibran@gmail.com>
2023-02-14 06:40:10 -0500

    assignment

--- Array1.c
+++ Array1.c
@@ -1,39 +1,29 @@
 #include <stdio.h>
-int Sum(int n);
-int main()
-{
-	int num, i;
-	printf("enter number: ");
-	scanf("%d", &num);
-	int flag =0;
-	for(i=2;i<=num/2;++i)
-	{
-		if(sum(i)==1){
-			if(sum(num-i) ==1){
-				printf("\nthe given %d can be expressed as the sum of %d and %d",num,i,num -i);
-				flag=1;
-			}
+#include <stdbool.h>
+bool isPrime(int n){
+	if(n<=1){
+		return false;
+	}
+	for(int i=2;i*i<=n;i++){
+		if(n%i ==0)
+		{
+			return false;
 		}
 	}
-	if (flag ==0)
-	printf("the given %d cannot be expressedas the sum of two prime numbers", num);
+	return true;
+}
+int main(){
+	int n;
+	printf("enter a positive integer: ");
+	scanf("%d",&n);
+	int count =0;
+	printf("%d=\n",n);
+	for(int i=2;i<=n/2;i++){
+		if(isPrime(i) && isPrime(n-i)){
+			printf("%d +%d\n",i,n-i);
+			count++;
+		}
+	}
+	printf("NoofWays =%d\n",count);
 	return 0;
 }
-int sum(int n)
-{
-	int i, isPrime=1;
-	if(n ==0 || n ==1){
-		isPrime =0;
-	}
-	else{
-
-	for(i=2;i<=n/2;++i){
-		if(n%i ==0){
-		break;
-	}
-}
-}
-return isPrime;
-
-}
-
